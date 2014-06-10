43de504107a20b629ccdf00507e743f747f336f2
moorecj <moore.cyril@gmail.com>
2014-06-10 14:19:27 -0400

    Initial commit: first test written, does not compile

---
+++ GameOfLifeRulesKata/GameOfLifeRules.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLifeRulesKata
+{
+    [Flags]
+    public enum State : int { Dead = 0, Alive = 1};
+    
+    public class GameOfLifeRules
+    {
+        
+    }
+}
---
+++ GameOfLifeRulesTest/GameOfLifeRulesTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using GameOfLifeRulesKata;
+
+
+namespace GameOfLifeRulesTest
+{
+    [TestFixture]
+    public class GameOfLifeRulesTests
+    {
+        [Test]
+        public void ADeadCellWithNoNeighbors_ShouldResultInNoNeighbors()
+        {
+            int numberOfNeighbors = 0;
+            Assert.That( GameOfLifeRules.ApplyRules( numberOfNeighbors ), Is.EqualTo( State.Dead ));
+
+        }
+
+    }
+}
